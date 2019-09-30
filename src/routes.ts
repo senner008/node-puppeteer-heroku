a7d0b51d7ac7ac78e0d58e7953e8b3a280349e37
nig <nig@beepanalytics.com>
2019-09-30 14:48:15 +0200

    WIP

--- src/routes.ts
+++ src/routes.ts
@@ -11,27 +11,30 @@ function decode(s) {
 export default function setRoutes (func) {
  express()
   .get('/foods', async function get(req, res) {
-    res.writeHead(200, {
-      "Content-Type": "application/json; charset=utf-8"
-    });
+
     try {
+      var memcachier : string = "local";
       var list;
       if (process.env.PORT) {
         list = await client.get('hello');
-        list[1] = "retrieved from cache";
+        memcachier = "retrieved from cache";
       } else {
         list = await func();
       }
-      if ((!list || !list.value) && process.env.PORT) {
+      if (!(Array.isArray(list) || !list.value) && process.env.PORT) {
           list = await func();
           var expire = 100000;
           await client.set('hello', list, {expires:expire});
-          list[1] = "setting cache to expire in " + expire;
+          memcachier = "setting cache to expire in " + expire;
       }
       if (req.query.id) {
         list = list[0].foods.filter(f => decode(f.title) == decode(req.query.id));
         if (list.length === 0) throw "invalid query parameter";
       }
+      res.writeHead(200, {
+        "Content-Type": "application/json; charset=utf-8",
+        "Memcachier" : memcachier
+      });
    
     } catch (err) {
       res.end(JSON.stringify({ err: err }))
