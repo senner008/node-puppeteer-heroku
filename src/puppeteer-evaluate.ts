948c1f81a5fcb72eeea8adc1ac87dd90660e5b61
senner008 <nielshtg@gmail.com>
2019-10-01 01:32:59 +0200

    fix, add strong types

--- src/puppeteer-evaluate.ts
+++ src/puppeteer-evaluate.ts
@@ -1,19 +1,41 @@
-export default async function extractList(page) {
-    return await page.evaluate((data) => {
+interface food {
+    title : string;
+    description : string | string[];
+    price : string | string[];
+}
 
-        var titles = document.querySelectorAll(".menu-list__title");
-        var obj = { foods: [] };
+interface foodtype {
+    title : string;
+    content : food[];
+}
+
+interface foodsObject {
+    foods : foodtype[];
+}
+
+export default async function extractList(page) {
+    return await page.evaluate((data) : [foodsObject] => {
+
+        const titles : NodeListOf<HTMLHeadingElement> = document.querySelectorAll(".menu-list__title");
+        var obj : foodsObject = { foods: [] };
         Array.from(titles).forEach(title => {
-            var lis = Array.from(title.nextElementSibling.nextElementSibling.getElementsByTagName("li"))
+            const food : food[] = Array.from(title.nextElementSibling.nextElementSibling.getElementsByTagName("li"))
                 .map(li => {
-                    var findtitle = li.querySelector(".item_title") || li.querySelector(".menu-list__item-title");
-                    return {
-                        "title": findtitle.innerHTML,
-                        "description": li.querySelector(".desc__content").innerHTML,
-                        "price": li.querySelector(".menu-list__item-price").innerHTML
+                    const findtitle  = (sel1 : HTMLSpanElement, sel2 : HTMLHeadingElement ) : string => (sel1 || sel2).innerHTML;
+
+                    var findElems = (sel: NodeListOf<HTMLParagraphElement | HTMLSpanElement>) : string[] | string => {
+                        var xelem = Array.from(sel);
+                        return xelem.length > 1 ? xelem.map(el => el.innerHTML) : xelem[0].innerHTML
                     }
+  
+                    return {
+                        "title": findtitle(li.querySelector<HTMLSpanElement>(".item_title"), li.querySelector<HTMLHeadingElement>(".menu-list__item-title")),
+                        "description": findElems(li.querySelectorAll<HTMLParagraphElement>(".desc__content")),
+                        "price": findElems(li.querySelectorAll<HTMLSpanElement>(".menu-list__item-price"))
+                    } as food
                 });
-            obj.foods.push({ title: title.innerHTML, content: lis });
+            const hasObject : boolean = obj.foods.some(s => s.title == title.innerHTML);   
+            obj.foods.push({ title: (hasObject ? 'Familie-' + title.innerHTML : title.innerHTML), content: food });
         });
         return [obj];
     })
