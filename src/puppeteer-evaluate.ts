be42011af905b9474af7936c9b0779a9f1a5201d
senner008 <nielshtg@gmail.com>
2019-10-02 05:05:48 +0200

    WIP

--- src/puppeteer-evaluate.ts
+++ src/puppeteer-evaluate.ts
@@ -45,42 +45,35 @@ export async function extractList(page, selectors) : Promise<foodsObject> {
             var xelem = Array.from(sel);
             return xelem.length > 1 ? xelem.map(el => el.innerHTML) : xelem[0].innerHTML
         }
-        function determineNestedArrayLength (obj : food) : undefined[] {
-            var arr : undefined[] = [];
+        function determineNestedArrayLength (obj : food) : food[] | null {
             for (let f in obj) {
                 if (Array.isArray(obj[f])) {
-                    arr = Array.from({length : obj[f].length});
-                    break;
+                    return Array(obj[f].length).fill(obj);
                 }
             }
-            return arr;
-        }
-        function extractToArray (arr : undefined[], obj : food) : foodStrict[] | null {
-            if (arr.length > 0) {
-                var arrayFood : foodStrict[] = arr.map((u : undefined ,index : number ) => {
-                    var clone : food = JSON.parse(JSON.stringify(obj))
-                    for (let c in clone) {
-                        clone[c] = Array.isArray(clone[c]) ? clone[c].filter((val : string, ind: number ) => ind === index)[0] : clone[c]
-                    }
-                    return clone as foodStrict;
-                });
-                return arrayFood; 
-            } 
             return null;
+        }
+        function divideInArray (arr : food[]) : foodStrict[] { 
+            return arr.map((food : food, index : number ) => {
+                var clone : food = JSON.parse(JSON.stringify(food))
+                for (let c in clone) {
+                    clone[c] = Array.isArray(clone[c]) ? clone[c].filter((val : string, ind: number) => ind === index)[0] : clone[c]
+                }
+                return clone as foodStrict;
+            }); 
         }
 
         Array.from(titles).forEach(title => {
             const food : (foodStrict | foodStrict[])[] = Array.from(title.nextElementSibling.nextElementSibling.getElementsByTagName("li"))
-                .map((li, index) => {
+                .map((li) => {
                     
                     const foodobj : food =  {
                         "title": findtitle(li.querySelector<HTMLSpanElement>(data.Title), li.querySelector<HTMLHeadingElement>(data.TitleAlt)),
                         "description": findElems(li.querySelectorAll<HTMLParagraphElement>(data.Content)),
                         "price": findElems(li.querySelectorAll<HTMLSpanElement>(data.Price))
                     }
-                    var emptyArray = determineNestedArrayLength(foodobj);
-                    var arrayFoodObjs : foodStrict[] | null = extractToArray(emptyArray, foodobj);
-                    return arrayFoodObjs ? arrayFoodObjs : foodobj as foodStrict;
+                    var foodArray = determineNestedArrayLength(foodobj);
+                    return foodArray ? divideInArray(foodArray) : foodobj as foodStrict;
                 });
             
             const hasObject : boolean = obj.some(s => s.title == title.innerHTML);  
